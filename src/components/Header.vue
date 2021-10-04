f46e13716660403daab59f6fbaad401f5ed0f568
adriensoulie <minishoes31400@gmail.com>
2021-10-05 00:26:06 +0700

    feature: created header

---
+++ src/components/Header.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="header">
+        <div class="header-app">
+            <h1>SchwiftyApp</h1>
+            <img alt="Rick and Morty Portal" src="../assets/Rick-And-Morty.png" style='width: 120px'>
+        </div>
+        <a href=''>
+            Login
+        </a>
+    </div>
+</template>
+
+
+<style scoped>
+.header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 'full';
+    padding: 10px;
+    margin: 0;
+}
+
+.header-app {
+    display: flex;
+    align-items: center;
+}
+
+h1 {
+    color: black;
+}
+
+a {
+    padding: 5px 20px 5px 20px;
+    background-color:	white;
+    border: 1px solid black;
+    color: black;
+    font-weight: 800;
+    transition: 0.3s;
+    border-radius: 4px;
+    text-decoration: inherit;
+}
+
+a:hover {
+    border: 1px solid transparent;
+    background-color: #97ce4c;
+    color: white;
+}
+
+.logo {
+    align-items: center;
+}
+
+</style>
