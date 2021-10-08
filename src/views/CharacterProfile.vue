6149ced42cc1e87b103a79c72f950920344a2bce
adriensoulie <minishoes31400@gmail.com>
2021-10-08 18:10:32 +0700

    fix: removed hover effect

--- src/views/CharacterProfile.vue
+++ src/views/CharacterProfile.vue
@@ -108,11 +108,6 @@ export default ({
         background-color: lemonchiffon;
         width: fit-content;
     }
-    .card:hover {
-        box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
-        transition: transform 125ms;
-        transform: translateY(-10px);
-    }
     .card-infos {
         display: block;
         width: 300px;
